fcec9c5d0a7b7a4b50722d961c9c31070266a558
Chrysanthus-Jumaa <160774354+Chrysanthus-Jumaa@users.noreply.github.com>
2025-08-13 18:17:05 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,6 +1,20 @@
+SELECT checkNumber,  paymentDate, amount
+FROM payments;
+
 SELECT orderDate, requiredDate, status_of_orders
 FROM ORDERS
 WHERE status_of_orders = 'In Process'
 ORDER BY orderDate DESC;
 
-SELECT
+SELECT firstName, lastName, email
+FROM employees
+WHERE job_title = 'Sales Rep'
+ORDER BY employeeNumber DESC;
+
+SELECT * FROM offices;
+
+SELECT productName, quantityInStock
+FROM products
+LIMIT 5
+ORDER BY buyPrice ASC;
+
